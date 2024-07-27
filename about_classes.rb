a7d754a8822b46745ad15725f7aa3925b719f1df
AimanK <aimankayad@gmail.com>
2024-07-27 15:44:33 -0400

    Save Progress on About Classes

--- about_classes.rb
+++ about_classes.rb
@@ -6,7 +6,7 @@ class AboutClasses < Neo::Koan
 
   def test_instances_of_classes_can_be_created_with_new
     fido = Dog.new
-    assert_equal __, fido.class
+    assert_equal Dog, fido.class
   end
 
   # ------------------------------------------------------------------
@@ -19,10 +19,10 @@ class AboutClasses < Neo::Koan
 
   def test_instance_variables_can_be_set_by_assigning_to_them
     fido = Dog2.new
-    assert_equal __, fido.instance_variables
+    assert_equal [], fido.instance_variables
 
     fido.set_name("Fido")
-    assert_equal __, fido.instance_variables
+    assert_equal [:@name], fido.instance_variables
   end
 
   def test_instance_variables_cannot_be_accessed_outside_the_class
@@ -115,15 +115,23 @@ class AboutClasses < Neo::Koan
 
   def test_initialize_provides_initial_values_for_instance_variables
     fido = Dog6.new("Fido")
-    assert_equal __, fido.name
+    assert_equal "Fido", fido.name
   end
 
   def test_args_to_new_must_match_initialize
-    assert_raise(___) do
+    assert_raise(NoMethodError) do
       Dog6.new
     end
     # THINK ABOUT IT:
     # Why is this so?
+
+=begin
+    The number of args to new must match
+    the initialization function/method
+    also referred to as the constructor
+    for the class Dog6
+
+=end
   end
 
   def test_different_objects_have_different_instance_variables
